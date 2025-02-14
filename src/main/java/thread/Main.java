ab1f093e07dfb5ff23182c32c07cf5096dff8f09
adipukr <dipu.kumar@50hertz.in>
2025-02-14 18:35:36 +0530

    new changes

--- src/main/java/thread/Main.java
+++ src/main/java/thread/Main.java
@@ -1,7 +1,12 @@
 package thread;
 
+import java.util.concurrent.ThreadFactory;
+
 public class Main {
-	public static void main(final String[] args) throws Exception {
-		
+	public static void main(final String[] args) {
+		if (args.length != 0)
+			System.out.println(args[0]);
+		ThreadFactory factory = Thread.ofVirtual().factory();
+		factory.newThread(() -> main(new String[] {"hello"})).start();;
 	}
 }
